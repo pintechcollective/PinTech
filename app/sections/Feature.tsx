65f2cd3bce0cb2ebe142c001858a7f08a55a40b8
Oscar Leal <oscarleal234@gmail.com>
2025-01-12 21:27:21 -0600

    added motion

--- app/sections/Feature.tsx
+++ app/sections/Feature.tsx
@@ -36,16 +36,24 @@ const Feature = ({
       className="border border-white/30 px-5 py-10 text-center rounded-xl flex-1 sm:max-w-sm relative "
     >
       <motion.div
-        className="absolute inset-0 border-2 border-[#9cd633] rounded-xl"
+        className="absolute inset-0 border-4 border-blue-500 rounded-xl"
         style={{
           WebkitMaskImage: maskImage,
           maskImage,
         }}
         ref={border}
       ></motion.div>
-      <div className="inline-flex h-14 w-14 bg-white text-black justify-center items-center rounded-lg">
+      <motion.div
+        className="inline-flex h-14 w-14 bg-white text-black justify-center items-center rounded-lg"
+        initial={{
+          opacity: 0,
+        }}
+        whileInView={{
+          opacity: 1,
+        }}
+      >
         <BsRobot />
-      </div>
+      </motion.div>
       <div>
         <h3 className="mt-6 font-bold">{title}</h3>
         <p className="mt-2 text-white/70">{description}</p>
