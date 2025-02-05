cbd86ff909690d1f93c3c04172acdd77b1bbb345
Oscar Leal <oscarleal234@gmail.com>
2025-02-05 14:58:19 -0600

    ts error

--- app/pages/services/page.tsx
+++ app/pages/services/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ServicesGrid from '@/app/sections/Services/ServicesGrid';
+// import ServicesGrid from '@/app/sections/Services/ServicesGrid';
 import { Motion } from '@/app/sections/About/Motion';
 import ServicesAccordion from '@/app/sections/Services/ServicesAccordion';
 import Cards from '@/app/sections/Services/Cards';
