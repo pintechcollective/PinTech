01860a807c906ebb0efc52cbf4e0dadd8fa5058e
Oscar Leal <oscarleal234@gmail.com>
2025-02-13 13:24:47 -0600

    added meta viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+            <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+      />
       <body className="font-ClashDisplay antialiased">
         <Header />
         {children}
