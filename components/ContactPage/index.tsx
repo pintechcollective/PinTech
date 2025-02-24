a1c3a3aa5a3402f2f28f02663611dabc3a94a10b
Oscar Leal <oscarleal234@gmail.com>
2025-02-23 18:48:53 -0600

    added a text area on form

--- components/ContactPage/index.tsx
+++ components/ContactPage/index.tsx
@@ -131,6 +131,22 @@ const Form = () => {
                 </label>
               </div>
             </div>
+            <div className="relative z-0 w-full mb-5 group">
+              <textarea
+                name="floating_textarea"
+                id="floating_textarea"
+                className="block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                rows={4}
+                required
+              />
+              <label
+                htmlFor="floating_textarea"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transhtmlForm -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                How Can We Help?
+              </label>
+            </div>
             <button
               type="submit"
               className="clip-diagonal bg-accent text-sm w-full sm:w-auto px-5 py-2.5 text-center"
