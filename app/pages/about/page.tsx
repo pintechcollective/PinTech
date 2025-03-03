c2c90851981e96528b975d3dbabaf2cc008b7401
Oscar Leal <oscarleal234@gmail.com>
2025-03-03 16:46:37 -0600

    added correct image for about metadata

--- app/pages/about/page.tsx
+++ app/pages/about/page.tsx
@@ -26,7 +26,7 @@ export const metadata = {
     type: 'website',
     images: [
       {
-        url: '/icons/PT - WORDMARK TM - BLUE.svg',
+        url: '/icons/PT - LogoMark - DBLBL.svg',
         width: 800,
         height: 600,
         alt: 'Pinnacle Technology Collective Logo',
