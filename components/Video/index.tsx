cc9e5b6a6256e556f057338500618ab86a1ba139
Oscar Leal <oscarleal234@gmail.com>
2025-03-15 21:13:35 -0500

    fix for mobile view

--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -1,36 +1,43 @@
-'use client'
+'use client';
 import anime from 'animejs';
 import BackgroundVideo from 'next-video/background-video';
 import mockVideo from '/videos/MockLoadInLogo.mp4.json';
 import React, { useEffect, useState } from 'react';
+// import Image from 'next/image';
+// import Logo from '@/public/icons/PT - LogoMark - DBLBL.svg';
+
 interface VideoProps {
   finishLoading: () => void;
 }
-const Video: React.FC<VideoProps> = ({finishLoading}) => {
-const [isMounted,setMounted]=useState(false);
+const Video: React.FC<VideoProps> = ({ finishLoading }) => {
+  const [isMounted, setMounted] = useState(false);
 
-const animate=()=>{
-    const loader=anime.timeline({
-        complete:()=> finishLoading()
-    })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const animate = () => {
+    const loader = anime.timeline({
+      complete: () => finishLoading(),
+    });
 
     loader.add({
-        targets: "#video",
-        delay:0,
-        scale:0,
-        duration:7000,
-        easing: "easeInOutExpo"
-    })
-}
-useEffect(()=>{
-const timeout= setTimeout(()=>setMounted(true),10)
-animate()
-return ()=>clearTimeout(timeout)
-}, [isMounted])
-    
+      targets: '#video',
+      delay: 0,
+      scale: 0,
+      duration: 7000,
+      easing: 'easeInOutExpo',
+    });
+  };
+  useEffect(() => {
+    const timeout = setTimeout(() => setMounted(true), 10);
+    animate();
+    return () => clearTimeout(timeout);
+  }, [animate, isMounted]);
+
   return (
-    <BackgroundVideo src={mockVideo} id="video" className="fixed top-0 bottom-0 w-screen h-screen object-cover">
-    </BackgroundVideo>
+    <BackgroundVideo
+      src={mockVideo}
+      id="video"
+      className="fixed top-0 left-0 w-full h-full object-contain lg:object-cover min-h-screen min-w-screen"
+    ></BackgroundVideo>
   );
 };
 
