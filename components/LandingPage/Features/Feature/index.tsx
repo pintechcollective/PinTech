65beca9f6d917ae6e821df07681c36694d9fafe9
Oscar Leal <oscarleal234@gmail.com>
2025-05-04 20:29:03 -0500

    added new footers

--- components/LandingPage/Features/Feature/index.tsx
+++ components/LandingPage/Features/Feature/index.tsx
@@ -40,12 +40,7 @@ const Feature = ({
   }, []);
 //allow
  return (
-    <>
-    
- <div
-      key={id}
-      className="border border-white/30 px-8 py-12 text-center rounded-xl flex-1 sm:max-w-lg relative"
-    >
+ <div key={id}className="border border-white/30 px-8 py-12 text-center rounded-xl flex-1 sm:max-w-lg relative">
       <motion.div
         className="absolute inset-0 border-4 border-blue-500 rounded-xl"
         style={{
@@ -72,7 +67,6 @@ const Feature = ({
         <p className="mt-2 text-white/70">{description}</p>
       </div>
     </div>
-    </>
   );
 };
 
